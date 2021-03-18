b13ed4eeb8686736a3f0f13a7b19efb3b241a970
pixelboogie <41452340+pixelboogie@users.noreply.github.com>
2021-03-18 10:19:37 -0700

    Add files via upload

---
+++ Lambda.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lambda : MonoBehaviour
+{
+
+    public delegate void TestDelegate();
+    private TestDelegate testDelegateFunction;
+
+    void Start()
+    {
+        testDelegateFunction = () => { Debug.Log("Lambda Expression"); };
+        testDelegateFunction();
+    }
+}
